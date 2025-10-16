8a3339721542f770db021a24111ded09a99796e4
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-10-16 11:26:43 +0200

    first commit

---
+++ Person.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace ovningsuppgift_automatiskt_namn_och_mejl;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Domain { get; set; } = "ankeborg.se";
+    public string Email
+    {
+        get { return $"{FirstName.ToLower()}.{LastName.ToLower()}@{Domain}"; }
+        set
+        {
+            string input = value.Trim();
+            int dot = input.IndexOf('.');
+            int at = input.IndexOf('@');
+            if (dot > 0 && at > dot)
+            {
+                FirstName = input[..dot];
+                LastName = input[(dot + 1)..at];
+                Domain = input[(at + 1)..];
+            }
+            else
+            {
+                throw new ArgumentException(
+                    "Ogiltig e-postadress format. Använd formatet: förnamn.efternamn@domän"
+                );
+            }
+        }
+    }
+
+    // Konstruktor för att mata in förnamn och efternamn vid skapandet av objektet
+    public Person(string firstName, string lastName)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ovningsuppgift_automatiskt_namn_och_mejl;
+
+var person = new Person("Kalle", "Anka");
+PrintPerson(person);
+
+person.FirstName = "Kajsa";
+PrintPerson(person);
+
+person.FirstName = "Jan";
+person.LastName = "Långben";
+person.Domain = "spoonerville.goof";
+PrintPerson(person);
+
+Console.WriteLine();
+Console.Write("Ange en mejl: ");
+Console.ForegroundColor = ConsoleColor.Cyan;
+person.Email = Console.ReadLine() ?? "";
+Console.ResetColor();
+PrintPerson(person);
+
+static void PrintPerson(Person person)
+{
+    Console.Write($"{person.FirstName} {person.LastName} har följande mejl: ");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine(person.Email);
+    Console.ResetColor();
+}
